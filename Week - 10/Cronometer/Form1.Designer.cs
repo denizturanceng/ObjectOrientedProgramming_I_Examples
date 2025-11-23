9b0316e5c990e027a2e0c1a1860cc87fa58dd0ad
deniz <denizturanceng@gmail.com>
2025-11-23 23:54:49 +0300

    23 november
    deniz first commit

---
+++ Week - 10/Cronometer/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblSayac = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSayac
+            // 
+            this.lblSayac.AutoSize = true;
+            this.lblSayac.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSayac.Location = new System.Drawing.Point(489, 223);
+            this.lblSayac.Name = "lblSayac";
+            this.lblSayac.Size = new System.Drawing.Size(315, 82);
+            this.lblSayac.TabIndex = 0;
+            this.lblSayac.Text = "00:00:00";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(805, 389);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(117, 56);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(345, 388);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(135, 57);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(584, 388);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(135, 57);
+            this.btnRestart.TabIndex = 3;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1250, 767);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblSayac);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSayac;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Week - 10/Cronometer/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kronometre
+{
+    public partial class Form1 : Form
+    {
+        private TimeSpan time = TimeSpan.Zero;     // Geçen süreyi tutan değişken (00:00:00)
+
+        private DateTime startTime;
+        // Kronometrenin başladığı anı tutar. 
+        // Burada neden "private DateTime startTime = new DateTime();" değil de sadece 
+        // "private DateTime startTime;" kullandık?
+        // Çünkü kronometrenin başlangıç zamanı her Start butonuna basıldığında 
+        // DateTime.Now ile güncelleniyor. Eğer "new DateTime()" deseydik, değişken 
+        // 01.01.0001 00:00:00 tarihine ayarlanırdı ve kronometrenin hesaplamaları 
+        // yanlış olurdu. Bu yüzden başlangıç değerini otomatik vermiyoruz; 
+        // gerçek başlangıç zamanını Start butonuna tıklanınca atıyoruz.
+
+
+        private bool isStarted = false;            // Kronometre çalışıyor mu kontrolü için
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSayac.Text = "00:00:00";            // Program açıldığında sayaç sıfırdan başlasın
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            // Timer her tetiklendiğinde (Interval kadar) burası çalışır (yani her saniye tetikleniyor.)
+            if (isStarted)
+            {
+                // Şu anki zaman - başlangıç zamanı = geçen süre
+                time = DateTime.Now - startTime;
+
+                // Tek haneli olan saat/dakika/saniyelerin başına "0" eklemek için formatlama
+                string hours = time.Hours.ToString("00");
+                string minutes = time.Minutes.ToString("00");
+                string seconds = time.Seconds.ToString("00");
+
+                // Label üzerinde süreyi göster
+                lblSayac.Text = hours + ":" + minutes + ":" + seconds;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Kronometre şu an çalışmıyorsa başlat
+            if (!isStarted)
+            {
+                isStarted = true;                  // Kronometre artık çalışıyor olarak işaretlenir
+
+                // Eğer kronometre durdurulup tekrar başlatılırsa kaldığı yerden devam etsin
+                startTime = DateTime.Now - time;
+
+                timer.Start();                     // Timer başlatılır
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Kronometre şu anda çalışıyorsa durdur
+            if (isStarted)
+            {
+                isStarted = false;                 // Kronometre durdu
+                timer.Stop();                      // Timer durdurulur
+
+                // Durdurulduğu ana kadar geçen süre hesaplanır
+                time = DateTime.Now - startTime;
+            }
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            // Restart: süreyi sıfırla + ekrana sıfır yaz
+            time = TimeSpan.Zero;                  // Süreyi baştan başlat
+            startTime = DateTime.Now;              // Yeni başlangıç zamanı şimdi
+            lblSayac.Text = "00:00:00";            // Ekranı sıfırla
+            // Not: restart'ta otomatik start yok, istersen buraya timer.Start() ekleyebilirsin
+        }
+
+    }
+}
